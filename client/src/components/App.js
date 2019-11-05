0ba3ce310417a9552ecbd46d4758f8867331fd85
Oleksandr Zhynzher <Oleksandr_Zhynzher@epam.com>
2019-11-05 12:31:33 +0200

    Add scss loaders and PostCSS to WebPack

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-
+import './App.scss'
 
 class App extends Component {
     render() {
         return (
             <div>
+                <h1>My Reacasdast App</h1>
+                <h1>My Reacasdast App</h1>
+                <h1>My Reacasdast App</h1>
                 <h1>My Reacasdast App</h1>
             </div>
         )
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -16,13 +17,33 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                     "css-loader",
+                     {
+                        loader: "postcss-loader",
+                        options: {
+                            config: {
+                                path: "./postcss.config.js"
+                            }
+                        }
+                    }
+                ]
+            },
+           
         ]
     },
 
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "index.css"
         })
     ]
 }
